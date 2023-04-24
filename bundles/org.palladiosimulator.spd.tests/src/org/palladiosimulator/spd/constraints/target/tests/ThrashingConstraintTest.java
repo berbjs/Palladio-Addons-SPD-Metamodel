7d50fef29dd847af98a5711089143fdacdd501c6
Floriment Klinaku <florimentklinaku@Floriments-MacBook-Pro.local>
2023-04-24 09:04:15 +0200

    missing versioned files

---
+++ bundles/org.palladiosimulator.spd.tests/src/org/palladiosimulator/spd/constraints/target/tests/ThrashingConstraintTest.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright 2022 by palladiosimulator.org
+ */
+package org.palladiosimulator.spd.constraints.target.tests;
+
+import org.palladiosimulator.spd.constraints.target.TargetFactory;
+import org.palladiosimulator.spd.constraints.target.ThrashingConstraint;
+import org.palladiosimulator.spd.constraints.tests.TemporalConstraintTest;
+
+import junit.textui.TestRunner;
+
+/**
+ * <!-- begin-user-doc -->
+ * A test case for the model object '<em><b>Thrashing Constraint</b></em>'.
+ * <!-- end-user-doc -->
+ * @generated
+ */
+public class ThrashingConstraintTest extends TemporalConstraintTest {
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public static void main(String[] args) {
+		TestRunner.run(ThrashingConstraintTest.class);
+	}
+
+	/**
+	 * Constructs a new Thrashing Constraint test case with the given name.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	public ThrashingConstraintTest(String name) {
+		super(name);
+	}
+
+	/**
+	 * Returns the fixture for this Thrashing Constraint test case.
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @generated
+	 */
+	@Override
+	protected ThrashingConstraint getFixture() {
+		return (ThrashingConstraint) fixture;
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#setUp()
+	 * @generated
+	 */
+	@Override
+	protected void setUp() throws Exception {
+		setFixture(TargetFactory.eINSTANCE.createThrashingConstraint());
+	}
+
+	/**
+	 * <!-- begin-user-doc -->
+	 * <!-- end-user-doc -->
+	 * @see junit.framework.TestCase#tearDown()
+	 * @generated
+	 */
+	@Override
+	protected void tearDown() throws Exception {
+		setFixture(null);
+	}
+
+} //ThrashingConstraintTest
