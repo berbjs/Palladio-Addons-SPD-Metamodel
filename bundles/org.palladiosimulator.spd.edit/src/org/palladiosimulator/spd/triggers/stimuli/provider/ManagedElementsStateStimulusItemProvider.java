6dbf81508e6b7afdc6816c78c5f1b2694816517e
Jens Berberich <st161898@stud.uni-stuttgart.de>
2024-09-24 21:38:59 +0200

    Fix stimuli labels being too generic

--- bundles/org.palladiosimulator.spd.edit/src/org/palladiosimulator/spd/triggers/stimuli/provider/ManagedElementsStateStimulusItemProvider.java
+++ bundles/org.palladiosimulator.spd.edit/src/org/palladiosimulator/spd/triggers/stimuli/provider/ManagedElementsStateStimulusItemProvider.java
@@ -24,93 +24,108 @@ import org.palladiosimulator.spd.triggers.stimuli.StimuliPackage;
  * @generated
  */
 public class ManagedElementsStateStimulusItemProvider extends TargetGroupStateStimulusItemProvider {
-	/**
-	 * This constructs an instance from a factory and a notifier.
-	 * <!-- begin-user-doc --> <!--
-	 * end-user-doc -->
-	 * @generated
-	 */
-	public ManagedElementsStateStimulusItemProvider(AdapterFactory adapterFactory) {
-		super(adapterFactory);
-	}
+    /**
+     * This constructs an instance from a factory and a notifier. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     * 
+     * @generated
+     */
+    public ManagedElementsStateStimulusItemProvider(AdapterFactory adapterFactory) {
+        super(adapterFactory);
+    }
 
-	/**
-	 * This returns the property descriptors for the adapted class.
-	 * <!-- begin-user-doc --> <!--
-	 * end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
-		if (itemPropertyDescriptors == null) {
-			super.getPropertyDescriptors(object);
+    /**
+     * This returns the property descriptors for the adapted class. <!-- begin-user-doc --> <!--
+     * end-user-doc -->
+     * 
+     * @generated
+     */
+    @Override
+    public List<IItemPropertyDescriptor> getPropertyDescriptors(Object object) {
+        if (itemPropertyDescriptors == null) {
+            super.getPropertyDescriptors(object);
 
-			addAggregationOverElementsPropertyDescriptor(object);
-		}
-		return itemPropertyDescriptors;
-	}
+            addAggregationOverElementsPropertyDescriptor(object);
+        }
+        return itemPropertyDescriptors;
+    }
 
-	/**
-	 * This adds a property descriptor for the Aggregation Over Elements feature. <!--
-	 * begin-user-doc --> <!-- end-user-doc -->
-	 *
-	 * @generated
-	 */
-	protected void addAggregationOverElementsPropertyDescriptor(Object object) {
-		itemPropertyDescriptors.add(createItemPropertyDescriptor(
-				((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(), getResourceLocator(),
-				getString("_UI_ManagedElementsStateStimulus_aggregationOverElements_feature"),
-				getString("_UI_PropertyDescriptor_description",
-						"_UI_ManagedElementsStateStimulus_aggregationOverElements_feature",
-						"_UI_ManagedElementsStateStimulus_type"),
-				StimuliPackage.Literals.MANAGED_ELEMENTS_STATE_STIMULUS__AGGREGATION_OVER_ELEMENTS, true, false, false,
-				ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
-	}
+    /**
+     * This adds a property descriptor for the Aggregation Over Elements feature. <!--
+     * begin-user-doc --> <!-- end-user-doc -->
+     *
+     * @generated
+     */
+    protected void addAggregationOverElementsPropertyDescriptor(Object object) {
+        itemPropertyDescriptors
+            .add(createItemPropertyDescriptor(((ComposeableAdapterFactory) adapterFactory).getRootAdapterFactory(),
+                    getResourceLocator(), getString("_UI_ManagedElementsStateStimulus_aggregationOverElements_feature"),
+                    getString("_UI_PropertyDescriptor_description",
+                            "_UI_ManagedElementsStateStimulus_aggregationOverElements_feature",
+                            "_UI_ManagedElementsStateStimulus_type"),
+                    StimuliPackage.Literals.MANAGED_ELEMENTS_STATE_STIMULUS__AGGREGATION_OVER_ELEMENTS, true, false,
+                    false, ItemPropertyDescriptor.GENERIC_VALUE_IMAGE, null, null));
+    }
 
-	/**
-	 * This returns the label text for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc
-	 * -->
-	 *
-	 * @generated NOT
-	 */
-	@Override
-	public String getText(final Object object) {
-		final StringBuilder stringBuilder = new StringBuilder(this.getString("_UI_CPUUtilization_type"));
-		final AGGREGATIONMETHOD labelValue = ((ManagedElementsStateStimulus) object).getAggregationOverElements();
-		if (labelValue != null && labelValue.toString().length() != 0) {
-			stringBuilder.append(" ").append(labelValue.toString());
-		}
-		stringBuilder.append(this.getRoleText(object));
-		return stringBuilder.toString();
-	}
+    /**
+     * This returns the label text for the adapted class. <!-- begin-user-doc --> <!-- end-user-doc
+     * -->
+     *
+     * @generated NOT
+     */
+    @Override
+    public String getText(final Object object) {
+        return getTextForStimulusName(object, this.getString("_UI_ManagedElementsStateStimulus_type"));
+    }
 
-	/**
-	 * This handles model notifications by calling {@link #updateChildren} to update any cached
-	 * children and by creating a viewer notification, which it passes to {@link #fireNotifyChanged}.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	public void notifyChanged(Notification notification) {
-		updateChildren(notification);
+    /**
+     * This is a helper function for constructing the label text for the adapted class.
+     * 
+     * @param object
+     * @param name
+     *            The name of the stimulus, can be obtained with {@link #getString(String)}
+     * @return
+     */
+    protected String getTextForStimulusName(final Object object, final String name) {
+        final StringBuilder stringBuilder = new StringBuilder(name);
+        final AGGREGATIONMETHOD labelValue = ((ManagedElementsStateStimulus) object).getAggregationOverElements();
+        if (labelValue != null && labelValue.toString()
+            .length() != 0) {
+            stringBuilder.append(" ")
+                .append(labelValue.toString());
+        }
+        stringBuilder.append(this.getRoleText(object));
+        return stringBuilder.toString();
+    }
 
-		switch (notification.getFeatureID(ManagedElementsStateStimulus.class)) {
-		case StimuliPackage.MANAGED_ELEMENTS_STATE_STIMULUS__AGGREGATION_OVER_ELEMENTS:
-			fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
-			return;
-		}
-		super.notifyChanged(notification);
-	}
+    /**
+     * This handles model notifications by calling {@link #updateChildren} to update any cached
+     * children and by creating a viewer notification, which it passes to
+     * {@link #fireNotifyChanged}. <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    @Override
+    public void notifyChanged(Notification notification) {
+        updateChildren(notification);
 
-	/**
-	 * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children
-	 * that can be created under this object.
-	 * <!-- begin-user-doc --> <!-- end-user-doc -->
-	 * @generated
-	 */
-	@Override
-	protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
-		super.collectNewChildDescriptors(newChildDescriptors, object);
-	}
+        switch (notification.getFeatureID(ManagedElementsStateStimulus.class)) {
+        case StimuliPackage.MANAGED_ELEMENTS_STATE_STIMULUS__AGGREGATION_OVER_ELEMENTS:
+            fireNotifyChanged(new ViewerNotification(notification, notification.getNotifier(), false, true));
+            return;
+        }
+        super.notifyChanged(notification);
+    }
+
+    /**
+     * This adds {@link org.eclipse.emf.edit.command.CommandParameter}s describing the children that
+     * can be created under this object. <!-- begin-user-doc --> <!-- end-user-doc -->
+     * 
+     * @generated
+     */
+    @Override
+    protected void collectNewChildDescriptors(Collection<Object> newChildDescriptors, Object object) {
+        super.collectNewChildDescriptors(newChildDescriptors, object);
+    }
 
 }
